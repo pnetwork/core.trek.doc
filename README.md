d45cf4cd54ef68caf958bd1897e0cc43be54acc2
Cello Hsueh <Cello1124@gmail.com>
2020-04-17 16:41:20 +0800

    feature: Add doc of blcks sdk

--- README.md
+++ README.md
@@ -11,16 +11,18 @@ Pentium Internet (奔騰網路) 提供多種開發環境，你可以透過 CLI �
 
 ## How to build documents
 
-使用 Spinx 產生文件，需先安裝相關套件
+使用 Spinx 產生文件，需先安裝相關套件，**建議使用 venv 保持環境乾淨**
 
     $ pip install -r requirements-docs.txt
 
 執行 `build.sh` 產出文件
 
 ```bash
+# 執行說明
+$ sh build.sh -h
 # 產生文件
 $ sh build.sh
 # 先 build mflow 後再一併產生文件
-$ sh build.sh 1
+$ sh build.sh --mflow
 ```
 
--- build.sh
+++ build.sh
@@ -1,8 +1,40 @@
 #!/usr/bin/env bash
 DIR=core.trek.doc
 MFLOW_DIR=core.tool.python.mflow
-BUILD_MFLOW=$1
-BUILD_EXT=$2
+BLKCS_SDK_DIR=core.sdk.python.blcks
+BUILD_MFLOW="0"
+BUILD_EXT="0"
+BUILD_BLCKS="0"
+
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "Document Gererator"
+      echo " "
+      echo "options:"
+      echo "-h, --help                show brief help"
+      echo "--mflow                   build from mflow repo"
+      echo "--vse                     build from Vscode extension repo"
+      echo "--blcks                   build from blcks sdk repo"
+      exit 0
+      ;;
+    --mflow)
+      BUILD_MFLOW="1"  
+      shift
+      ;;
+    --vse)
+      BUILD_EXT="1"
+      shift
+      ;;
+    --blcks)
+      BUILD_BLCKS="1"  
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
 
 make clean
 
@@ -16,6 +48,20 @@ if [ "$BUILD_MFLOW" == "1" ]; then
     cd ../../$DIR
     cp -r ../$MFLOW_DIR/docs/reference/commands ./source/reference/clikit/
     # cp ../$MFLOW_DIR/docs/guide/quickstart.rst ./source/reference/clikit/quickstart.rst
+fi
+
+# copy references from blcks sdk repo
+if [ "$BUILD_BLCKS" == "1" ]; then
+    echo "build document of blcks sdk"
+    cd ../$BLKCS_SDK_DIR
+    pip install -r requirements-docs.txt
+    python setup.py build
+    python setup.py install
+    cd docs
+    make html
+    cd ../../$DIR
+    # cp -r ../$BLKCS_SDK_DIR/docs/reference/clients ./source/reference/blcks
+    cp -r ../$BLKCS_SDK_DIR/docs/reference/services ./source/reference/blcks
 fi
 
 # copy references from extension repo
