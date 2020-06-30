cd7a7820617d2c6f9821b470b079b8db61e70025
chelsealopentinum <chelsea.lo@pentium.network>
2020-06-30 17:47:41 +0800

    misc: test

--- build.sh
+++ build.sh
@@ -46,7 +46,7 @@ make clean
 if [ "$BUILD_TREK" = "1" ]; then
     echo "git clone trek repo"
     cd submodule
-    # git submodule update --init --recursive $TREK_DIR
+    git submodule update --init --recursive $TREK_DIR
     echo "build document of trek"
     cd $TREK_DIR
     pip install -r requirements-docs.txt
@@ -65,7 +65,7 @@ fi
 if [ "$BUILD_BLCKS" = "1" ]; then
     echo "git clone blcks sdk repo"
     cd submodule
-    # git submodule update --init --recursive $BLKCS_SDK_DIR
+    git submodule update --init --recursive $BLKCS_SDK_DIR
     echo "build document of blcks sdk"
     cd $BLKCS_SDK_DIR
     pip install -r requirements-docs.txt
@@ -89,7 +89,7 @@ fi
 if [ "$BUILD_EXT" = "1" ]; then
     echo "git clone vscode extension repo"
     cd submodule
-    # git submodule update --init --recursive $VSCODE_EXTENSION_DIR
+    git submodule update --init --recursive $VSCODE_EXTENSION_DIR
     echo "build document of trek vscode extension"
     cd $VSCODE_EXTENSION_DIR
     pip install -r requirements-docs.txt
