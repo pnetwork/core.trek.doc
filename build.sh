2e6b2a12fdc5c7e44e9dd68f654843ca7bba2c45
chelsealopentinum <chelsea.lo@pentium.network>
2020-07-07 15:00:19 +0800

    fix: remove submodule -> to git clone repo with token

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ BUILD_TREK="0"
 BUILD_EXT="0"
 BUILD_BLCKS="0"
 CRAWL_SCRIPT_WF="0"
+GITHUB_TOKEN=""
+REF_REPO_BR="master"
 
 while test $# -gt 0; do
   case "$1" in
@@ -20,6 +22,7 @@ while test $# -gt 0; do
       echo "--vse                     build from VSCode extension repo"
       echo "--blcks                   build from blcks sdk repo"
       echo "--no-script-wf            do not crawl script and workflow template from nexus server"
+      echo "--token                   use github token to checkout reference repo (core.sdk.python.blcks, core.tool.python.mflow, core.vscode.extension.mflow)"
       exit 0
       ;;
     --trek)
@@ -38,6 +41,14 @@ while test $# -gt 0; do
       CRAWL_SCRIPT_WF="1"  
       shift
       ;;
+    --token )
+      GITHUB_TOKEN="$2"
+      if ! [ "$GITHUB_TOKEN" ]; then
+        echo "Please input the github token for checkout private repo."
+        exit 1
+      fi
+      shift
+      ;;
     *)
       break
       ;;
@@ -46,17 +57,24 @@ done
 
 make clean
 
+clone_repo(){
+    repo_name=$1
+    br_name=$2
+    rm -rf ${repo_name}
+    echo "$repo_name"
+    if [ "$GITHUB_TOKEN" ]; then
+        git clone -b ${br_name} https://${GITHUB_TOKEN}@github.com/pnetwork/${repo_name}.git
+    else
+        git clone -b ${br_name} https://github.com/pnetwork/${repo_name}.git
+    fi
+}
+
 # copy references from trek repo
-if [ "$BUILD_TREK" = "1" ]; then
+if [ "$BUILD_TREK" == "1" ]; then
     echo "git clone trek repo"
-    br=$(git config -f .gitmodules --get submodule.submodule/$TREK_DIR.branch)
-    cd submodule/$TREK_DIR
-    echo "git checkout branch $br"
-    git checkout -b $br
-    echo "git fetch branch $br"
-    git fetch origin $br
-    git reset --hard FETCH_HEAD
-    git clean -df
+    cd reference_repos
+    clone_repo ${TREK_DIR} ${REF_REPO_BR}
+    cd $TREK_DIR
     echo "build document of trek"
     pip install -r requirements-docs.txt
     cd docs
@@ -65,22 +83,17 @@ if [ "$BUILD_TREK" = "1" ]; then
     make html
     cd ../../../
     rm -rf ./source/reference/cli/*
-    cp -r submodule/$TREK_DIR/docs/reference/commands ./source/reference/cli/
-    cp submodule/$TREK_DIR/CHANGELOG.md ./source/reference/cli/CHANGELOG.md
+    cp -r reference_repos/$TREK_DIR/docs/reference/commands ./source/reference/cli/
+    cp reference_repos/$TREK_DIR/CHANGELOG.md ./source/reference/cli/CHANGELOG.md
 fi
 
 
 # copy references from blcks sdk repo
 if [ "$BUILD_BLCKS" = "1" ]; then
     echo "git clone blcks sdk repo"
-    br=$(git config -f .gitmodules --get submodule.submodule/$BLKCS_SDK_DIR.branch)
-    cd submodule/$BLKCS_SDK_DIR
-    echo "git checkout branch $br"
-    git checkout -b $br
-    echo "git fetch branch $br"
-    git fetch origin $br
-    git reset --hard FETCH_HEAD
-    git clean -df
+    cd reference_repos
+    clone_repo ${BLKCS_SDK_DIR} ${REF_REPO_BR}
+    cd $BLKCS_SDK_DIR
     echo "build document of blcks sdk"
     pip install -r requirements-docs.txt
     python setup.py build
@@ -92,24 +105,19 @@ if [ "$BUILD_BLCKS" = "1" ]; then
     cd ../../../
     rm -rf ./source/reference/blcks/*
     # cp -r ../$BLKCS_SDK_DIR/docs/reference/clients ./source/reference/blcks
-    cp -r submodule/$BLKCS_SDK_DIR/docs/_static/images ./source/_static/
-    cp -r submodule/$BLKCS_SDK_DIR/docs/reference/services ./source/reference/blcks
-    cp submodule/$BLKCS_SDK_DIR/docs/guide/installation.rst ./source/reference/blcks/installation.rst
-    cp submodule/$BLKCS_SDK_DIR/docs/guide/configuration.rst ./source/reference/blcks/configuration.rst
-    cp submodule/$BLKCS_SDK_DIR/docs/guide/quickstart.rst ./source/reference/blcks/quickstart.rst
+    cp -r reference_repos/$BLKCS_SDK_DIR/docs/_static/images ./source/_static/
+    cp -r reference_repos/$BLKCS_SDK_DIR/docs/reference/services ./source/reference/blcks
+    cp reference_repos/$BLKCS_SDK_DIR/docs/guide/installation.rst ./source/reference/blcks/installation.rst
+    cp reference_repos/$BLKCS_SDK_DIR/docs/guide/configuration.rst ./source/reference/blcks/configuration.rst
+    cp reference_repos/$BLKCS_SDK_DIR/docs/guide/quickstart.rst ./source/reference/blcks/quickstart.rst
 fi
 
 # copy references from trek vscode extension repo
 if [ "$BUILD_EXT" = "1" ]; then
     echo "git clone vscode extension repo"
-    br=$(git config -f .gitmodules --get submodule.submodule/$VSCODE_EXTENSION_DIR.branch)
-    cd submodule/$VSCODE_EXTENSION_DIR
-    echo "git checkout branch $br"
-    git checkout -b $br
-    echo "git fetch branch $br"
-    git fetch origin $br
-    git reset --hard FETCH_HEAD
-    git clean -df
+    cd reference_repos
+    clone_repo ${VSCODE_EXTENSION_DIR} ${REF_REPO_BR}
+    cd $VSCODE_EXTENSION_DIR
     echo "build document of trek vscode extension"
     pip install -r requirements-docs.txt
     cd docs
@@ -118,8 +126,8 @@ if [ "$BUILD_EXT" = "1" ]; then
     make html
     cd ../../../
     rm -rf ./source/reference/extension/*
-    cp -r submodule/$VSCODE_EXTENSION_DIR/docs/reference/commands ./source/reference/extension/commands
-    cp submodule/$VSCODE_EXTENSION_DIR/CHANGELOG.md ./source/reference/extension/CHANGELOG.md
+    cp -r reference_repos/$VSCODE_EXTENSION_DIR/docs/reference/commands ./source/reference/extension/commands
+    cp reference_repos/$VSCODE_EXTENSION_DIR/CHANGELOG.md ./source/reference/extension/CHANGELOG.md
 fi
 
 # crawl script and workflow template on nexus server
